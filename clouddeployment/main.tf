c5940009d45f1fa6c6f3b58f5f581ad7444905e0
Sardor Abdullaev <sardorabdullaev91@gmail.com>
2022-02-10 17:37:46 +0100

    fix aws security

--- clouddeployment/main.tf
+++ clouddeployment/main.tf
@@ -23,10 +23,11 @@ resource "aws_instance" "app_server" {
   ami                     = "ami-00bf0e20ed7ea8cdc"
   instance_type           = "t2.micro"
   subnet_id               = aws_subnet.private[0].id
-  security_groups         = [aws_security_group.securitygroup.id]
+  security_groups         = [aws_security_group.securitygroup.name]
   key_name                = aws_key_pair.ssh.key_name
   disable_api_termination = false
   ebs_optimized           = false
+  hibernation             = false
   tags                    = {
     "Name" = var.app_name
   }
@@ -39,27 +40,32 @@ resource "aws_instance" "app_node" {
   ami                     = "ami-00bf0e20ed7ea8cdc"
   instance_type           = "t2.micro"
   subnet_id               = aws_subnet.private[0].id
-  security_groups         = [aws_security_group.securitygroup.id]
+  security_groups         = [aws_security_group.securitygroup.name]
   key_name                = aws_key_pair.ssh.key_name
   disable_api_termination = false
   ebs_optimized           = false
   tags                    = {
     "Name" = var.app_name
   }
+  hibernation = false
   credit_specification {
     cpu_credits = "unlimited"
   }
 }
 
 resource "aws_instance" "ec2jumphost" {
-  instance_type           = "t2.micro"
+  instance_type           = "t2.nano"
   ami                     = "ami-00bf0e20ed7ea8cdc"
-  subnet_id                 = aws_subnet.public[0].id
-  security_groups         = [aws_security_group.securitygroup.id]
+  subnet_id               = aws_subnet.public[0].id
+  security_groups         = [aws_security_group.ssh_securitygroup.name]
   key_name                = aws_key_pair.ssh.key_name
   disable_api_termination = false
   ebs_optimized           = false
+  hibernation             = false
   tags                    = {
     "Name" = "${var.app_name}-jumphost"
+  }
+  credit_specification {
+    cpu_credits = "standard"
   }
 }
--- clouddeployment/security.tf
+++ clouddeployment/security.tf
@@ -8,9 +8,9 @@ resource "aws_key_pair" "ssh" {
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
-resource "aws_security_group" "securitygroup" {
-  name        = "ShhSecurityGroup"
-  description = "ShhSecurityGroup"
+resource "aws_security_group" "ssh_securitygroup" {
+  name        = "sshSecurityGroup"
+  description = "sshSecurityGroup"
   vpc_id      = aws_vpc.vpc.id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
@@ -25,6 +25,63 @@ resource "aws_security_group" "securitygroup" {
     protocol    = "-1"
   }
   tags = {
-    "Name" = "ShhSecurityGroup"
+    "Name" = "sshSecurityGroup"
+  }
+}
+
+resource "aws_security_group" "securitygroup" {
+  name        = "privateSecurityGroup"
+  description = "privateSecurityGroup"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    cidr_blocks = ["172.31.0.0/24"]
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["172.31.1.0/24"]
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["172.31.0.0/24"]
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["172.31.1.0/24"]
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["172.31.0.0/16"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["172.31.0.0/16"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = ["172.31.0.0/16"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["172.31.0.0/16"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+  tags = {
+    "Name" = "privateSecurityGroup"
   }
 }
