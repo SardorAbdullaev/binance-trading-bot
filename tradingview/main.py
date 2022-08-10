b1445ea518bbe720e3c7828edefc222981319254
Sardor Abdullaev <sardorabdullaev91@gmail.com>
2022-08-10 22:54:00 +0200

    make a long term bot add rasp pi

--- tradingview/main.py
+++ tradingview/main.py
@@ -14,23 +14,43 @@ sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(colorlog.ColoredFormatter(
     '%(log_color)s [%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s', datefmt='%a, %d %b %Y %H:%M:%S'))
 logger.addHandler(sh)
-os_metrics = ["RSI", "STOCH.K", "ADX", "Mom", "MACD", "Stoch.RSI", "W%R", "BBP"]
-moving_average_metrics = ["EMA10","SMA10","EMA50","SMA50","EMA200","SMA200","Ichimoku","VWMA","HullMA"]
+os_metrics_weights = {
+    "RSI": 8,
+    "ADX": 3,
+    "Mom": 5,
+    "MACD": 8,
+    "W%R": 5,
+    "BBP": 5
+}
+
+os_metrics = os_metrics_weights.keys()
+avg_metrics_weights = {
+    "EMA10": 1,
+    "SMA10": 1,
+    "EMA50": 2,
+    "SMA50": 3,
+    "EMA200": 8,
+    "SMA200": 5,
+    "Ichimoku": 5,
+    "VWMA": 5
+}
+moving_average_metrics = avg_metrics_weights.keys()
+
 
 def modify_summary(result):
-    for symbol,r in result.items():
+    for symbol, r in result.items():
         interim_summary = {
             "NEUTRAL": 0,
             "SELL": 0,
             "BUY": 0
         }
-        for name,decision in r["oscillators"]['COMPUTE'].items():
+        for name, decision in r["oscillators"]['COMPUTE'].items():
             if name in os_metrics:
-                interim_summary[decision] += 1
+                interim_summary[decision] += os_metrics_weights[name]
 
-        for name,decision in r["moving_averages"]['COMPUTE'].items():
+        for name, decision in r["moving_averages"]['COMPUTE'].items():
             if name in moving_average_metrics:
-                interim_summary[decision] += 1
+                interim_summary[decision] += avg_metrics_weights[name]
 
         if interim_summary["NEUTRAL"] >= interim_summary["SELL"]:
             if interim_summary["NEUTRAL"] > interim_summary["BUY"]:
@@ -43,6 +63,7 @@ def modify_summary(result):
             else:
                 interim_summary["RECOMMENDATION"] = "BUY"
         r["summary"] = interim_summary
+
 
 @app.route('/', methods=['GET'])
 def index():
